d02acacf3b30bcc9e3aeea4adff2ae377448c6be
Jerry11671 <jegoisgs66@gmail.com>
2023-09-09 07:27:00 +0100

    Commit with correction for utc time

--- core/views.py
+++ core/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
 
 @api_view(['GET',])
 def index(request):
+    current_time = datetime.now(pytz.utc)
+
     data = {
         "slack_name": request.GET['slack_name'],
         "current_day": datetime.now().strftime('%A'),
-        "utc_time": datetime.now(timezone.utc),
+        "utc_time": current_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "track": request.GET['track'],
         "github_file_url": "https://github.com/jerry11671/hng_task_one/tree/main/core/view.py",
         "github_repo_url": "https://github.com/jerry11671/hng_task_one.git",
--- task/settings.py
+++ task/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-vg9=3&q(@6cg98w!_2uj!(&v+=8od!5nmgx^i&3up5v2h&c7_f'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-vg9=3&q(@6cg98w!_2uj!(&v+=8od!5nmgx^i&3up5v2h&c7_f'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
